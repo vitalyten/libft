f0dee72baf63c516c0ba768790d015d2ac46e632
VitalyTen <vtenigin@student.42.us.org>
2016-10-01 15:28:04 -0700

    lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/28 20:31:13 by vtenigin          #+#    #+#             */
-/*   Updated: 2016/09/28 21:49:55 by vtenigin         ###   ########.fr       */
+/*   Updated: 2016/10/01 15:26:05 by vtenigin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,26 @@
 
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
-	t_list *ret;
-	t_list *cur;
-	t_list *prev;
+	t_list	*ret;
+	t_list	*cur;
+	t_list	*tmp;
 
 	ret = NULL;
-	if (lst)
+	while (lst)
 	{
-		ret = f(lst);
-		prev = ret;
-		lst = lst->next;
-		while (lst)
+		tmp = f(lst);
+		tmp->next = NULL;
+		if (!ret)
 		{
-			cur = f(lst);
-			prev->next = cur;
-			prev = cur;
-			lst = lst->next;
+			cur = tmp;
+			ret = tmp;
 		}
-		prev->next = NULL;
+		else
+		{
+			cur->next = tmp;
+			cur = cur->next;
+		}
+		lst = lst->next;
 	}
 	return (ret);
-	// t_list *ret;
-	// t_list *cur;
-	// t_list *prev;
-
-	// ret = NULL;
-	// if (lst)
-	// {
-	// 	ret = f(lst);
-	// 	prev = ret;
-	// 	lst = lst->next;
-	// 	while (lst)
-	// 	{
-	// 		cur = f(lst);
-	// 		prev->next = cur;
-	// 		prev = cur;
-	// 		lst = lst->next;
-	// 	}
-	// 	prev->next = NULL;
-	// }
-	// return (ret);
 }
